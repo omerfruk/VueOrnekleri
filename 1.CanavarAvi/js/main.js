635b5034c4866338a7b8a464eb805237f19ddb61
omerfruk <omer.fruk3547@gmail.com>
2021-04-29 23:20:23 +0300

    oyunun dosyaları yülendi

---
+++ 1.CanavarAvi/js/main.js
@@ -0,0 +1,17 @@
+new Vue({
+    el:"#app",
+    data:{
+        player_health:100,
+        monster_health:100,
+        gameIsOn:false,
+    },
+    methods :{
+        startGame : function(){
+            gameIsOn=true
+        },
+        playerAttact:function(){
+
+        }
+    }
+})
+ß
