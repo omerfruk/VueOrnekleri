27165b1266ae67fc825f7cc9d358b02a975f73ca
omerfruk <omer.fruk3547@gmail.com>
2021-04-29 23:52:26 +0300

    butonlar icin methodlar yazildi

--- 1.CanavarAvi/js/main.js
+++ 1.CanavarAvi/js/main.js
@@ -7,10 +7,31 @@ new Vue({
     },
     methods :{
         startGame : function(){
-            gameIsOn=true
+            this.gameIsOn=true;
+            console.log(this.gameIsOn);
         },
         playerAttact:function(){
-
+            var point=Math.ceil(Math.random()*10);
+            this.monsterAttact();
+            this.monster_health-=point
+        },
+        playerSpecialAttact:function(){
+            var point=Math.ceil(Math.random()*25);
+            this.monsterAttact();
+            this.monster_health-=point
+        },
+        monsterAttact:function(){
+            var point=Math.ceil(Math.random()*20);
+            this.player_health-=point
+        },
+        healUp:function(){
+            var point=Math.ceil(Math.random()*20);
+            this.monsterAttact()
+            this.player_health+=point;
+        },
+        giveUp:function(){
+            this.player_health=0;
+            
         }
     }
 })
