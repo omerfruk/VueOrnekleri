e76081ab27e824d74092b06c92f9d1bae886f3fb
omerfruk <omer.fruk3547@gmail.com>
2021-05-04 03:33:11 +0300

    verilerin degistirilip sol taraftaki alanda gosterilmesini sagladık

--- 3.FormIslemleri/src/App.vue
+++ 3.FormIslemleri/src/App.vue
@@ -14,32 +14,57 @@
                 <div class="col-md-12">
                   <div class="form-group">
                     <label for="email">Kullanıcı Adı</label>
-                    <input type="text" id="username" class="form-control">
+                    <input type="text"
+                           id="username"
+                           class="form-control"
+                           v-model.lazy.trim="userData.username">
                   </div>
+                  <!--
+                  model.lazy inputtan cikinca isleme aliyor
+                  model.number inputun number olmasini kontrol ediyor
+                  model.trim inputu bosluklardan trimliyor
+                  molde.lazy.number.trim olarak zincirleme de yapabiliyoruz
+                  -->
                   <div class="form-group">
                     <label for="password">Şifre</label>
-                    <input type="password" id="password" class="form-control">
+                    <input type="password"
+                           id="password"
+                           class="form-control"
+                           v-model.lazy.trim="userData.password">
                   </div>
                   <div class="form-group">
                     <label for="age">Yaş</label>
-                    <input type="number" id="age" class="form-control">
+                    <input type="number"
+                           id="age"
+                           class="form-control"
+                           v-model.lazy.trim.number="userData.age" >
                   </div>
                 </div>
               </div>
               <div class="row">
                 <div class="col-md-12 form-group">
                   <label for="message">Açıklama</label><br>
-                  <textarea id="message" rows="3" class="form-control"></textarea>
+                  <textarea
+                      id="message"
+                      rows="3" class="form-control"
+                      v-model="userData.message"
+                  ></textarea>
                 </div>
               </div>
               <div class="row">
                 <div class="col-md-12">
                   <div class="form-group">
                     <label>
-                      <input type="checkbox" value="yazilim"> Yazılım
+                      <input
+                          v-model="userData.interest"
+                          type="checkbox"
+                          value="yazilim"> Yazılım
                     </label>
                     <label>
-                      <input type="checkbox" value="donanim"> Donanım
+                      <input
+                          v-model="userData.interest"
+                          type="checkbox"
+                          value="donanim"> Donanım
                     </label>
                   </div>
 
@@ -48,25 +73,32 @@
               <div class="row">
                 <div class="col-md-12 form-group">
                   <label>
-                    <input type="radio" value="erkek"> Erkek
+                    <input v-model="userData.gender" type="radio" value="erkek"> Erkek
                   </label>
                   <label>
-                    <input type="radio" value="kadin"> Kadın
+                    <input v-model="userData.gender" type="radio" value="kadin"> Kadın
                   </label>
                 </div>
               </div>
               <div class="row">
                 <div class="col-md-12 from-group">
                   <label>Şehir</label>
-                  <select class="form-control">
-                    <option></option>
+                  <select v-model="userData.selectedCiti" class="form-control">
+                    <option :selected="city == 'adana'" v-for="citi in userData.cities">{{citi}}</option>
                   </select>
                 </div>
               </div>
+              <div class="row">
+                <div class="col-md-12 form-group">
+                  <appSwitch v-model="switched"></appSwitch>
+                </div>
+              </div>
               <hr>
+              <!-- @click.prevent butonun bir yere gondermesini engelliyor -->
               <div class="row">
                 <div class="col-md-12">
                   <button
+                      @click.prevent="submit()"
                     class="btn btn-primary">Gönder!
                   </button>
                 </div>
@@ -75,23 +107,24 @@
           </div>
         </div>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6" v-if="isSubmitted">
           <div class="panel panel-info">
             <div class="panel-heading">
               <h4>Form Verileri</h4>
             </div>
+            <!--style="white-space:pre;" multi line degerlerini gormemiz icin   -->
             <div class="panel-body">
-              <p>Kullanıcı Adı:</p>
-              <p>Şifre:</p>
-              <p>Yaş:</p>
-              <p>Açıklama: </p>
+              <p>Kullanıcı Adı:{{userData.username}}</p>
+              <p>Şifre:{{userData.password}}</p>
+              <p>Yaş: {{ userData.age }}</p>
+              <p style="white-space:pre; ">Açıklama: {{userData.message}}</p>
               <p><strong>İlgi Alanları</strong></p>
               <ul>
-                <li></li>
+                <li v-for="item in userData.interest">{{item}}</li>
               </ul>
-              <p>Cinsiyet:</p>
-              <p>Şehir:</p>
-              <p>Toggle:</p>
+              <p>Cinsiyet: {{userData.gender}}</p>
+              <p>Şehir:{{userData.selectedCiti}}</p>
+              <p>Toggle:{{switched}}</p>
             </div>
           </div>
       </div>
@@ -101,7 +134,35 @@
 </template>
 
 <script>
-  export default {}
+
+ import Switch from "./Switch";
+
+  export default {
+    components:{
+      appSwitch:Switch
+    },
+    data (){
+      return{
+        userData:{
+          username:'',
+          password:'',
+          age:null,
+          message:'',
+          interest:[],
+          gender:'',
+          cities:['istanbul','adana','izmir'],
+          selectedCiti:'',
+        },
+        switched:true,
+        isSubmitted:false,
+      }
+    },
+    methods:{
+      submit(){
+        this.isSubmitted=true;
+      }
+    },
+  }
 </script>
 
 <style>
