3b140afd925d89474810ab64b3dcd59c4ff6e3e5
omerfruk <omer.fruk3547@gmail.com>
2021-05-04 05:08:05 +0300

    form islemlerine 2. ornek yuklendi ve npm modulleri silindi(alan tasarrufu icin)

---
+++ 3.FormIslemleri/ornek2/src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container ">
+    <form v-if="!isPush">
+      <div class="row ">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <Form v-model="userdata.fullName"></Form>
+          <div class="form-group">
+            <label for="email">mail</label>
+            <input type="email"
+                   id="email"
+                   class="form-control"
+                   v-model.lazy.trim="userdata.email">
+          </div>
+          <div class="form-group">
+            <label for="password">Şifre</label>
+            <input type="password"
+                   id="password"
+                   class="form-control"
+                   v-model.lazy.trim="userdata.password">
+          </div>
+          <div class="row">
+            <div class="col-md-12 form-group">
+              <label>
+                <input v-model="userdata.gender" type="radio" value="erkek"> Erkek
+              </label>
+              <label>
+                <input v-model="userdata.gender" type="radio" value="kadin"> Kadın
+              </label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <button
+                  @click.prevent="Push()"
+                  class="btn btn-primary">Gönder!
+              </button>
+            </div>
+          </div>
+
+
+          <!-- Soru 2 -->
+          <!-- Üye Ol formu, Submit EDİLMEDİĞİ SÜRECE ekranda gözükmelidir.-->
+          <!-- Üye Bilgileri Ekranı ise form Submit EDİLDİKTEN SONRA ekranda gözükecektir -->
+
+          <!-- Soru 3 -->
+          <!-- Yukarıdaki uygulamayı düzenleyerek Tam Ad (Ad + Soyad) alanı için bir Form Kontrol Elemanı üretiniz -->
+        </div>
+      </div>
+    </form>
+    <hr>
+    <div class="row" v-if="isPush">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-info">
+          <div class="panel-heading">
+            <h4>Girdiğiniz Bilgier</h4>
+          </div>
+          <div class="panel-body">
+            <p>Tam Ad: {{ userdata.fullName }}</p>
+            <p>E-mail: {{userdata.email}} </p>
+            <p>Şifre: {{userdata.password}}</p>
+            <p>Cinsiyet: {{userdata.gender}} </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Form from "./Form";
+export default {
+  components:{
+    Form,
+  },
+  data() {
+    return {
+      userdata: {
+        fullName:'omer faruk',
+        email: '',
+        password: '',
+        gender: '',
+      },
+      isPush: false,
+
+    }
+  },
+  methods:{
+    Push(){
+      this.isPush=true
+    }
+  }
+}
+</script>
---
+++ 3.FormIslemleri/ornek2/src/Form.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="form-group">
+      <label>Isim</label>
+      <input type="text"
+             class="form-control"
+             :value="firsName"
+      @input="nameChange(true,$event)">
+    </div>
+    <div class="form-group">
+      <label>Soyisim</label>
+      <input type="text"
+             class="form-control"
+             :value="lastName"
+      @input="nameChange(false,$event)">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  computed: {
+    firsName() {
+      return this.value.split(" ")[0];
+    },
+    lastName() {
+      return this.value.split(" ")[1];
+    }
+  },
+  methods:{
+    nameChange(isFirst,event){
+      let FullName="";
+      if (isFirst){
+        FullName=event.target.value +' '+ this.lastName;
+      }else{
+        FullName=this.firsName +' '+ evet.target.value;
+      }
+      this.value=FullName;
+      this.$emit("input",this.value)
+    }
+  }
+}
+
+</script>
+
