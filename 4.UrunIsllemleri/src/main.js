c67dec354f980b9cd8e78c1bbfc7b0de47abec0d
omerfruk <omer.fruk3547@gmail.com>
2021-05-23 17:24:08 +0300

    main js resource kelendi

--- 4.UrunIsllemleri/src/main.js
+++ 4.UrunIsllemleri/src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import {router} from "./router";
+import {store} from "./store/store";
+import VueResource from "vue-resource"
+
+Vue.use(VueResource);
 
 new Vue({
     el: '#app',
     render: h => h(App),
-    router
+    router,
+    store
 })
