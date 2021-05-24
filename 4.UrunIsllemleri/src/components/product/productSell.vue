b42b0ef96ac4bd374d684b9882ec0aa0e90d8728
omerfruk <omer.fruk3547@gmail.com>
2021-05-24 21:06:06 +0300

    loading bekleme anı yapildi

--- 4.UrunIsllemleri/src/components/product/productSell.vue
+++ 4.UrunIsllemleri/src/components/product/productSell.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="container">
+    <div class="loading" :style="isLoading">
+      <div class="lds-ripple">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
     <div class="row">
       <div class="col-6 offset-3 pt-3 card mt-5 shadow">
         <div class="card-body">
@@ -8,7 +14,7 @@
           <div class="form-group">
             <label>Ürün Adı</label>
             <select class="form-control" v-model="selectedProduct" @change="productSelected">
-              <option selected disabled>lütfen bir seçenek seçin </option>
+              <option selected disabled>lütfen bir seçenek seçin</option>
               <option
                   :disabled="product.count==0"
                   :value="product.key"
@@ -23,19 +29,19 @@
                 <div class="col-12 text-center">
                   <div class="mb-3">
                     <span class="badge badge-info">Stok : {{ product.count }}</span>
-                    <span class="badge badge-primary">Fiyat : {{ product.price | currency}}</span>
+                    <span class="badge badge-primary">Fiyat : {{ product.price | currency }}</span>
                   </div>
-                  <p class="border border-warning p-2 text-secondary">{{product.description}}</p>
+                  <p class="border border-warning p-2 text-secondary">{{ product.description }}</p>
                 </div>
               </div>
             </div>
           </div>
           <div class="form-group">
             <label>Adet</label>
-            <input type="text" class="form-control" placeholder="Ürün adetini giriniz..">
+            <input type="text" v-model="product_count" class="form-control" placeholder="Ürün adetini giriniz..">
           </div>
           <hr>
-          <button class="btn btn-primary">Kaydet</button>
+          <button @click="save" class="btn btn-primary">Kaydet</button>
         </div>
       </div>
     </div>
@@ -46,19 +52,51 @@
 import {mapGetters} from "vuex"
 
 export default {
-  data(){
+  data() {
     return {
       selectedProduct: null,
-      product :null
+      product: null,
+      product_count: null,
+      saveButtonCliced:false,
     }
   },
   computed: {
-    ...mapGetters(["getProducts"])
+    ...mapGetters(["getProducts"]),
+    isLoading(){
+      if(this.saveButtonCliced){
+        return{
+          display :"block"
+        }
+      }else {
+        return {
+          display: "none"
+        }
+      }
+    }
   },
-  methods:{
-    productSelected(){
+  methods: {
+    productSelected() {
       this.$store.getters.getProduct();
-      this.product =this.$store.getters.getProduct(this.selectedProduct)[0];
+      this.product = this.$store.getters.getProduct(this.selectedProduct)[0];
+    },
+    save() {
+      this.saveButtonCliced=true;
+      let product = {
+        key: this.selectedProduct,
+        count: this.product_count
+      }
+      this.$store.dispatch("sellProduct", product)
+    }
+  },
+  beforeRouteLeave(to, from, next) {
+    if ((this.selectedProduct !== null || this.product_count)&& !this.saveButtonCliced) {
+      if (confirm("kaydedilmemiş veriler var çıkmak istiyormusunuz")) {
+        next();
+      } else {
+        next(false);
+      }
+    } else {
+      next();
     }
   }
 }
