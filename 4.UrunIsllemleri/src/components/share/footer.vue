a8cf04c3654b269018e4dce860e66fec228072b4
omerfruk <omer.fruk3547@gmail.com>
2021-05-24 14:57:40 +0300

    footerde veriler degistirildi

--- 4.UrunIsllemleri/src/components/share/footer.vue
+++ 4.UrunIsllemleri/src/components/share/footer.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar fixed-bottom  navbar-light" style="background-color: #e3f2fd;">
     <span class="navbar-text mr-auto">
-        <strong>vuejsegitim.com</strong> | gkandemir
+        <strong>MardinLtd.Şti</strong> | omer Faruk Tasdemir
     </span>
     <span class="navbar-text navbar-nav my-2 my-lg-0 mr-3">
         <strong>Alış Tutarı : </strong>
