94a2c25b82d108866dd598b35153b4ecde26db7d
omerfruk <omer.fruk3547@gmail.com>
2021-05-24 13:53:46 +0300

    urun fiyatlarının duzenlenmeis ssaglandi

--- 4.UrunIsllemleri/src/store/actions.js
+++ 4.UrunIsllemleri/src/store/actions.js
@@ -1,6 +1,19 @@
-export const setTradeResult = ({state}) => {
+import Vue from "vue";
 
+export const setTradeResult = ({state, commit}, tradeResult) => {
+   commit("updateTradeResult",tradeResult)
+   let tradeData={
+      purchase:state.purchase,
+      sale:state.sale
+   }
+   Vue.http.put("https://urun-islemleri-7f6bd-default-rtdb.firebaseio.com/trade-result.json",tradeData)
+       .then(response =>{
+       })
 }
-export const getTradeResult =({commit}) => {
-
+export const getTradeResult = ({commit}) => {
+Vue.http.get("https://urun-islemleri-7f6bd-default-rtdb.firebaseio.com/trade-result.json")
+    .then(response =>{
+        console.log(response)
+        commit("updateTradeResult",response.body)
+    })
 }
