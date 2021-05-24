32154c1cd0cb980a661f342df2a1829a170e1e15
omerfruk <omer.fruk3547@gmail.com>
2021-05-24 16:59:15 +0300

    import eklendi ve sayfa yönlendirilmesi yapildi

--- 4.UrunIsllemleri/src/store/modules/product.js
+++ 4.UrunIsllemleri/src/store/modules/product.js
@@ -1,4 +1,6 @@
 import Vue from "vue";
+import {router} from "../../router";
+
 const state = {
     products: []
 }
@@ -34,6 +36,7 @@ const actions = {
                     count: product.count
                 }
                 dispatch("setTradeResult",tradeResult )
+                router.replace("/")
             })
     },
     sellProduct({commit},peyload){
