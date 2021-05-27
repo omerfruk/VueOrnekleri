6e8d8076cbd823274e79c4700bf82dac3ab2d484
omerfruk <omer.fruk3547@gmail.com>
2021-05-27 15:09:57 +0300

    tag componenti olusturuldu

---
+++ 5.InputTaglar /src/components/tag.vue
@@ -0,0 +1,28 @@
+<template>
+ <span class="tag" >
+    <span class="content">{{ tag }}</span>
+    <span class="close" @click="">X</span>
+  </span>
+</template>
+
+<script>
+export default {
+  props : ["tag", "index"]
+}
+</script>
+
+<style scoped>
+.tag {
+  background-color: #fbbd08;
+  padding: 10px;
+  color: #000;
+  cursor: default;
+  font-size: 14px;
+  margin-right: 10px;
+}
+
+.tag .close {
+  font-size: 12px;
+  cursor: pointer;
+}
+</style>
